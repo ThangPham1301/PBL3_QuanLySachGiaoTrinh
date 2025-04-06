4fa7fc9d4c44619ff17c2debc2bd5880fc9797cb
Thang-Pham <hahanh29911301@gmail.com>
2025-04-06 15:47:07 +0700

    create Home,Login,...

---
+++ Book exchange/src/App.jsx
@@ -0,0 +1,24 @@
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from '../Components/HomePage';
+import LoginPage from '../Components/LoginPage';
+import AboutPage from '../Components/AboutPage';
+import ShopNowPage from '../Components/ShopNowPage';
+import './App.css'
+// import './index.css'
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Định tuyến trang Home là trang đầu tiên */}
+        <Route path="/" element={<HomePage />} />
+        {/* Định tuyến trang Login */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/about" element={<AboutPage />} /> 
+        <Route path="/shop" element={<ShopNowPage />} /> 
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
